edc526e5b26252aeec497f3b0f758c743992efad
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-10-02 19:57:45 +0200

    feat: :sparkles: add:  footer component

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const burgerBtn = document.querySelector(".nav__burger");
 const allNavLink = document.querySelectorAll(".nav__item");
 
 const handleNav = () => {
+  console.log("e");
+  burgerBtn.classList.toggle("active");
   allNavLink.forEach((item) => {
     item.classList.toggle("show");
   });
