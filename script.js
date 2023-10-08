2617686d7298c2d9bdaeb38fec5b37939aa7ba81
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-10-08 12:25:06 +0200

    feat: :sparkles: add: scrollSpy, dropdown nav, checking page height

--- script.js
+++ script.js
@@ -1,17 +1,78 @@
+/// dropdown naw + show nav on mobile
+
 const nav = document.querySelector(".nav");
 const burgerBtn = document.querySelector(".nav__burger");
 const allNavLink = document.querySelectorAll(".nav__item");
 
 const handleNav = () => {
-  console.log("e");
   burgerBtn.classList.toggle("active");
   allNavLink.forEach((item) => {
-    console.log(item);
-
     item.classList.toggle("show");
+    item.classList.toggle("nav-items-animation");
   });
 };
+
+const handleNavItemsAnimation = () => {
+  let delayTime = 0;
+  allNavLink.forEach((item) => {
+    item.style.animationDelay = "." + (delayTime + 2) + "s";
+    delayTime++;
+  });
+};
+
+handleNavItemsAnimation ()
 burgerBtn.addEventListener("click", handleNav);
+
+/// add position fixed on nav when user scroll down
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("navbar");
+  let isNavbarFixed = false;
+
+  const handleScroll = () => {
+    const scrollPos = window.scrollY;
+
+    if (scrollPos > 100 && !isNavbarFixed) {
+      navbar.classList.add("fixed"); // Dodaj klasę fixed, gdy użytkownik zjedzie trochę niżej
+      isNavbarFixed = true;
+    } else if (scrollPos <= 100 && isNavbarFixed) {
+      navbar.classList.remove("fixed"); // Usuń klasę fixed, gdy użytkownik wjedzie na samą górę strony
+      isNavbarFixed = false;
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+});
+
+/// scrollSpy
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("navbar");
+  const navLinks = navbar.querySelectorAll(".nav__item");
+  const sections = document.querySelectorAll("section");
+
+  const changeNavOnScroll = () => {
+    let fromTop = window.scrollY + navbar.offsetHeight + 1;
+
+    sections.forEach((section) => {
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (section.id === link.getAttribute("href").substring(1)) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  };
+
+  window.addEventListener("scroll", changeNavOnScroll);
+});
+
+/// render image on section 2
 
 const imageBoxes = document.querySelectorAll(".section2__box");
 
